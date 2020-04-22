42f01d8775e71e4ad8a2a49e5b655a3e508b2b12
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2020-04-22 17:06:56 -0400

    Generate the template ini file. (#3)
    * Generate the template ini file.
    
    * Remove the Github actions test for time being.
    
    Co-authored-by: Ben Bond-Lamberty <bondlamberty@pnnl.gov>

---
+++ R/data.R
@@ -0,0 +1,11 @@
+## License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+#' template_ini
+#'
+#' A character vector consisting of Hector inputs. 
+#'
+#' @format A vector 
+#' \describe{ A character vector where each element in the vector contains a different 
+#' line of hector ini file. 
+#' }
+'template_ini'
