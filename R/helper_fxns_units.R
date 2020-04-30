7fd0153332355ab9dab3b4904f9510ceb0b5aa2c
Kalyn <kalyn.dorheim@pnnl.gov>
2020-04-30 12:20:44 -0400

    Functions to make converting units easier

---
+++ R/helper_fxns_units.R
@@ -0,0 +1,45 @@
+#' Chemical symbol-aware unit conversion
+#'
+#' @param x Numeric value to convert
+#' @param from,to Units from/to which to convert. Syntax is identical to
+#'   [udunits2::ud.convert()] except that chemical symbols in hard brackets
+#'   (e.g. `[N2O]`) can be converted.
+#' @return Values of `x` converted to `to` units.
+#' @author Alexey Shiklomanov
+#' @export
+#' @examples
+#' ud_convert2(5, "Pg [C]", "Mt [CO2]")
+#' #> 18.32223
+ud_convert2 <- function(x, from, to) {
+    udunits2::ud.convert(x, parse_chem(from), parse_chem(to))
+}
+
+#' Chemical symbol-aware unit conversion
+#'
+#' @param unit a string of unit information and chemical information (C, N, N2O, ect.)
+#' @return a formated unit string
+#' @author Alexey Shiklomanov
+#' @noRd 
+parse_chem <- function(unit) {
+    unit2 <- unit
+    rx <- "\\[.*?\\]"
+    m <- regexpr(rx, unit2)
+    while (m != -1) {
+        regmatches(unit2, m) <- sprintf(
+            "(1/%f)",
+            get_molmass(regmatches(unit2, m))
+        )
+        m <- regexpr(rx, unit2)
+    }
+    unit2
+}
+
+#' Calculate the molar mass for a chemical species
+#'
+#' @param s a string of chemical compound or species
+#' @return the molar mass
+#' @author Alexey Shiklomanov
+#' @noRd
+get_molmass <- function(s) {
+    biogas::molMass(gsub("\\[|\\]", "", s))
+}
