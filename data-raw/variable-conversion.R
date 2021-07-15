5ebfd4448a84bf7d3002cac928ab47d1b7ab2bbe
kdorheim <kalyn.dorheim@pnnl.gov>
2021-07-15 13:07:14 -0400

    Update the raw data.

--- data-raw/variable-conversion.R
+++ data-raw/variable-conversion.R
@@ -3,11 +3,12 @@
 # Save the tables that will be used to convert emission and concentration time series 
 # to Hector inputs. 
 
-# For the CMIP6 inputs we use the conversion table created by the RCMIP project.
+# For the CMIP6 inputs we use the conversion table created by the RCMIP project see 
+# https://github.com/ashiklom/hector-rcmip/tree/master/inst for more details. 
 path <- here::here('data-raw', 'RCMIP_variable-conversion.csv') 
-rcmipCMIP6_conversion <- data.table::as.data.table(readr::read_csv(path))
+rcmipCMIP6_conversion <- read.csv(path, stringsAsFactors = FALSE)
 
-# Because of how RCMIP conversion table were formatted make sure that the strings do not start with a space. 
+# Because of how RCMIP conversion table was formatted make sure that the strings do not start with a space. 
 cols_to_modify <- which(names(rcmipCMIP6_conversion) %in% c("Model", "Scenario", "Region", "Variable", "Unit", "Mip_Era"))
 for(i in seq_along(cols_to_modify)){rcmipCMIP6_conversion[[i]] <- trimws(rcmipCMIP6_conversion[[i]], which = 'left')}
 
