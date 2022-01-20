bc3f81e007130f0ad2f5daac39b1820c4b26f5db
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2022-01-20 15:43:24 -0500

    Update hectorv3 (#20)
    idealized scenarios used in the ocean mip

--- tests/testthat/test_inis-run.R
+++ tests/testthat/test_inis-run.R
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+test_that('inis run', {
+  
+  ini_files <- list.files(INI_DIR, pattern = ".ini")
+  ini_files <- file.path(INI_DIR, ini_files)
+
+  # For each of the ini files, check to make sure that the core can be set up 
+  # and run. 
+  lapply(ini_files, function(f){
+    core <- hector::newcore(inifile = f)
+    expect_equal(class(core)[1], "hcore")
+    run_msg <- capture.output(hector::run(core = core, runtodate = 2100)) 
+    expect_equal(length(run_msg), 5)
+  })
+})
+||||||| fd87739
+=======
 test_that('inis run', {
   
   ini_files <- list.files(INI_DIR, pattern = ".ini")
@@ -12,3 +29,4 @@ test_that('inis run', {
     expect_equal(length(run_msg), 5)
   })
 })
+>>>>>>> master
